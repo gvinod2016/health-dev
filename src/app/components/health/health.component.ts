f57d3fd86c0312c4b3df786d6676c7eff783935b
Kumar <gkumarswamy16@gmail.com>
2023-07-25 18:17:23 +0530

    added location in question list without GEO access

--- src/app/components/health/health.component.ts
+++ src/app/components/health/health.component.ts
@@ -6,179 +6,54 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './health.component.html',
   styleUrls: ['./health.component.css']
 })
-export class HealthComponent  implements OnInit{
+export class HealthComponent implements OnInit {
 
-  stateName: string="";
-  pincode: number = 0
+  defaultStateName = "Telangana";
+  defaultPincode = "500072"
+  stateName: string = "";
+  pincode: string = ""
   quotations: any = null;
   u65Phone = 18775778518;
   medicalCare = 18665021292;
-    
-    constructor(private http: HttpClient) { }
-    ngOnInit(){
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position)=>{
-          console.log(position.coords.latitude, position.coords.longitude, 'geolocatoin...')
-          const url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude+"&sensor=true&key=AIzaSyATY1qYx3oJJi94U-lKaQTlpiIbmqbC6iw"
-          this.http.get(url).subscribe((res: any)=>{
-            console.log(res)
-            const apiRes = res?.results
-            if(apiRes){
-              console.log(apiRes, 'apiRes')
-              const locationDetails = apiRes.filter((row: any)=> row.types.includes('postal_code'))
-              
-              const address_components = locationDetails[0].address_components
-              console.log(locationDetails, 'locationDetails', address_components)
-              this.stateName = address_components[address_components.length - 2]?.long_name
-              this.pincode = address_components[0]?.long_name
-              this.quotations = {
-                q: 'Are you younger than 65?',
-                yes: {
-                  q: 'Do you have Medicare or Medicaid?',
-                  yes: {
-                    q: 'Confirming your zip code is ' + this.pincode,
-                    yes: {
-                      q: 'Can you take a phone call now?',
-                      yes: { type: 'call', phoneNo: this.u65Phone },
-                      no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                      type: 'q'
-                    },
-                    no: {
-                      q: 'Got it! Are you looking for coverage in ' + this.stateName,
-                      yes: {
-                        q: 'Can you take a phone call now?',
-                        yes: { type: 'call', phoneNo: this.u65Phone },
-                        no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        type: 'q'
-                      },
-                      no: {
-                        q: 'Can you take a phone call now?',
-                        yes: { type: 'call', phoneNo: this.u65Phone },
-                        no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        type: 'q'
-                      },
-                      type: 'q'
-                    },
-                    type: 'q'
-                  },
-                  no: {
-                    q: 'Confirming your zip code is '+ this.pincode,
-                    yes: {
-                      q: 'Can you take a phone call now?',
-                      yes: { type: 'call', phoneNo: this.u65Phone },
-                      no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                      type: 'q'
-                    },
-                    no: {
-                      q: 'Got it! Are you looking for coverage in ' + this.stateName,
-                      yes: {
-                        q: 'Can you take a phone call now?',
-                        yes: { type: 'call', phoneNo: this.u65Phone },
-                        no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        type: 'q'
-                      },
-                      no: {
-                        q: 'Can you take a phone call now?',
-                        yes: { type: 'call', phoneNo: this.u65Phone },
-                        no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        type: 'q'
-                      },
-                      type: 'q' 
-                    },
-                    type: 'q'
-                  },
-                  type: 'q'
-                },
-                no: {
-                  q: 'Do you have Medicare Parts A and B?	',
-                  yes: {
-                    q: 'Can you take a phone call now?',
-                    yes: { type: 'call', phoneNo: this.medicalCare },
-                    no: {
-                      q: 'Are you interested in learning more about your options online?',
-                      yes: {
-                        q: 'Confirming your zip code is ' + this.pincode,
-                        yes: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        no: {
-                          q: 'Got it! Are you looking for coverage in ' + this.stateName,
-                          yes: { q: 'To MediaAlpha clicks results', type: 'input' },
-                          no: {
-                            q: 'Can you take a phone call now?',
-                            yes: { type: 'call', phoneNo: this.medicalCare},
-                            no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                            type: 'q'
-                          },
-                          type: 'q'
-                        },
-                        type: 'q'
-                      },
-                      no: {
-                        q: 'Got it! Are you looking for coverage in ' + this.stateName,
-                        yes: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        no: {
-                          q: ' Can you take a phone call now?',
-                          yes: { type: 'call', phoneNo:  this.medicalCare },
-                          no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                          type: 'q'
-                        },
-                        type: 'q'
-                      },
-                      type: 'q'
-                    },
-                    type: 'q'
-                  },
-                  no: {
-                    q: 'Can you take a phone call now?',
-                    yes: { type: 'call', phoneNo:  this.medicalCare },
-                    no: {
-                      q: 'Are you interested in learning more about your options online?',
-                      yes: {
-                        q: 'Confirming your zip code is ' + this.pincode,
-                        yes: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        no: {
-                          q: 'Got it! Are you looking for coverage in ' + this.stateName,
-                          yes: { q: 'To MediaAlpha clicks results', type: 'input' },
-                          no: {
-                            q: 'Can you take a phone call now?',
-                            yes: { type: 'call', phoneNo:  this.medicalCare },
-                            no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                            type: 'q'
-                          },
-                          type: 'q'
-                        },
-                        type: 'q'
-                      },
-                      no: {
-                        q: 'Got it! Are you looking for coverage in ' + this.stateName,
-                        yes: { q: 'To MediaAlpha clicks results', type: 'input' },
-                        no: {
-                          q: 'Can you take a phone call now?',
-                          yes: { type: 'call', phoneNo:  this.medicalCare },
-                          no: { q: 'To MediaAlpha clicks results', type: 'input' },
-                          type: 'q'
-                        },
-                        type: 'q'
-                      },
-                      type: 'q'
-                    },
-                    type: 'q'
-                  },
-                  type: 'q'
-                },
-                type: 'q'
-              };
-            }
-          })
-        });
-      } else {
-        console.log('Geolocation is not supported by this browser.')
-      }
-    }
+
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    const url = "https://ipapi.co/json/"
+    this.http.get(url)
+      .subscribe({
+        next: (apiRes: any) => {
+          if (apiRes) {
+            this.stateName = apiRes?.region ? apiRes?.region : this.defaultStateName
+            this.pincode = apiRes?.postal ? apiRes?.postal : this.defaultPincode
+            this.quotations = this.getQuestionList()
+          } else {
+            this.stateName = this.defaultStateName
+            this.pincode = this.defaultPincode
+            this.quotations = this.getQuestionList()
+          }
+        },
+        error: (err) => {
+          console.log(err, 'error')
+          this.stateName = this.defaultStateName
+          this.pincode = this.defaultPincode
+          this.quotations = this.getQuestionList()
+        }
+      })
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition((position) => {
+    //     console.log(position.coords.latitude, position.coords.longitude, 'geolocatoin...')
+    //     // const url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude+"&sensor=true&key=AIzaSyATY1qYx3oJJi94U-lKaQTlpiIbmqbC6iw"
+
+    //   });
+    // } else {
+    //   console.log('Geolocation is not supported by this browser.')
+    // }
+  }
 
   onQuestionACtion(chlidNode: any) {
     console.log(chlidNode, 'chlidNode')
-    if(chlidNode.type == 'input'){
-      const url = 'https://medicareconsumer.com/u65-health-insurance/?location='+ this.pincode;
+    if (chlidNode.type == 'input') {
+      const url = 'https://medicareconsumer.com/u65-health-insurance/?location=' + this.pincode;
       window.open(url);
       // window.location.href = url
     } else {
@@ -186,5 +61,143 @@ export class HealthComponent  implements OnInit{
     }
 
 
+  }
+  getQuestionList() {
+    return {
+      q: 'Are you younger than 65?',
+      yes: {
+        q: 'Do you have Medicare or Medicaid?',
+        yes: {
+          q: 'Confirming your zip code is ' + this.pincode,
+          yes: {
+            q: 'Can you take a phone call now?',
+            yes: { type: 'call', phoneNo: this.u65Phone },
+            no: { q: 'To MediaAlpha clicks results', type: 'input' },
+            type: 'q'
+          },
+          no: {
+            q: 'Got it! Are you looking for coverage in ' + this.stateName,
+            yes: {
+              q: 'Can you take a phone call now?',
+              yes: { type: 'call', phoneNo: this.u65Phone },
+              no: { q: 'To MediaAlpha clicks results', type: 'input' },
+              type: 'q'
+            },
+            no: {
+              q: 'Can you take a phone call now?',
+              yes: { type: 'call', phoneNo: this.u65Phone },
+              no: { q: 'To MediaAlpha clicks results', type: 'input' },
+              type: 'q'
+            },
+            type: 'q'
+          },
+          type: 'q'
+        },
+        no: {
+          q: 'Confirming your zip code is ' + this.pincode,
+          yes: {
+            q: 'Can you take a phone call now?',
+            yes: { type: 'call', phoneNo: this.u65Phone },
+            no: { q: 'To MediaAlpha clicks results', type: 'input' },
+            type: 'q'
+          },
+          no: {
+            q: 'Got it! Are you looking for coverage in ' + this.stateName,
+            yes: {
+              q: 'Can you take a phone call now?',
+              yes: { type: 'call', phoneNo: this.u65Phone },
+              no: { q: 'To MediaAlpha clicks results', type: 'input' },
+              type: 'q'
+            },
+            no: {
+              q: 'Can you take a phone call now?',
+              yes: { type: 'call', phoneNo: this.u65Phone },
+              no: { q: 'To MediaAlpha clicks results', type: 'input' },
+              type: 'q'
+            },
+            type: 'q'
+          },
+          type: 'q'
+        },
+        type: 'q'
+      },
+      no: {
+        q: 'Do you have Medicare Parts A and B?	',
+        yes: {
+          q: 'Can you take a phone call now?',
+          yes: { type: 'call', phoneNo: this.medicalCare },
+          no: {
+            q: 'Are you interested in learning more about your options online?',
+            yes: {
+              q: 'Confirming your zip code is ' + this.pincode,
+              yes: { q: 'To MediaAlpha clicks results', type: 'input' },
+              no: {
+                q: 'Got it! Are you looking for coverage in ' + this.stateName,
+                yes: { q: 'To MediaAlpha clicks results', type: 'input' },
+                no: {
+                  q: 'Can you take a phone call now?',
+                  yes: { type: 'call', phoneNo: this.medicalCare },
+                  no: { q: 'To MediaAlpha clicks results', type: 'input' },
+                  type: 'q'
+                },
+                type: 'q'
+              },
+              type: 'q'
+            },
+            no: {
+              q: 'Got it! Are you looking for coverage in ' + this.stateName,
+              yes: { q: 'To MediaAlpha clicks results', type: 'input' },
+              no: {
+                q: ' Can you take a phone call now?',
+                yes: { type: 'call', phoneNo: this.medicalCare },
+                no: { q: 'To MediaAlpha clicks results', type: 'input' },
+                type: 'q'
+              },
+              type: 'q'
+            },
+            type: 'q'
+          },
+          type: 'q'
+        },
+        no: {
+          q: 'Can you take a phone call now?',
+          yes: { type: 'call', phoneNo: this.medicalCare },
+          no: {
+            q: 'Are you interested in learning more about your options online?',
+            yes: {
+              q: 'Confirming your zip code is ' + this.pincode,
+              yes: { q: 'To MediaAlpha clicks results', type: 'input' },
+              no: {
+                q: 'Got it! Are you looking for coverage in ' + this.stateName,
+                yes: { q: 'To MediaAlpha clicks results', type: 'input' },
+                no: {
+                  q: 'Can you take a phone call now?',
+                  yes: { type: 'call', phoneNo: this.medicalCare },
+                  no: { q: 'To MediaAlpha clicks results', type: 'input' },
+                  type: 'q'
+                },
+                type: 'q'
+              },
+              type: 'q'
+            },
+            no: {
+              q: 'Got it! Are you looking for coverage in ' + this.stateName,
+              yes: { q: 'To MediaAlpha clicks results', type: 'input' },
+              no: {
+                q: 'Can you take a phone call now?',
+                yes: { type: 'call', phoneNo: this.medicalCare },
+                no: { q: 'To MediaAlpha clicks results', type: 'input' },
+                type: 'q'
+              },
+              type: 'q'
+            },
+            type: 'q'
+          },
+          type: 'q'
+        },
+        type: 'q'
+      },
+      type: 'q'
+    };
   }
 }
